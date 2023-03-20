dd34ddca3aa3d296e8694075c31a1bf54ddbb283
Даниил Смирнов <barsukgeroi03@gmail.com>
2023-03-20 22:54:21 +0300

    Загрузка мульти клиента

---
+++ mytcpserver.h
@@ -0,0 +1,29 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+#include <QtNetwork>
+#include <QByteArray>
+#include <QDebug>
+#include <Qlist>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+
+    void slotServerRead();
+    //void slotReadClient();
+private:
+    QTcpServer * mTcpServer;
+    Qlist<QTcpSocket> * mTcpSocket;
+    int server_status;
+};
+#endif // MYTCPSERVER_H
