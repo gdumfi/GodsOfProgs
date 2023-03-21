d01e0c0202223aef8f177d79147ec175962c4bc1
gdumfi <44841897+gdumfi@users.noreply.github.com>
2023-03-21 09:51:20 +0300

    Update mytcpserver.cpp

--- mytcpserver.cpp
+++ mytcpserver.cpp
@@ -23,25 +23,28 @@ MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
 
 void MyTcpServer::slotNewConnection(){
     if(server_status==1){
-        QTcpSocket curr_mTcpSocket;
+        QTcpSocket* curr_mTcpSocket;
         curr_mTcpSocket = mTcpServer->nextPendingConnection();
         curr_mTcpSocket->write("Hello, World!!! I am echo server!\r\n");
         connect(curr_mTcpSocket, &QTcpSocket::readyRead,
                 this,&MyTcpServer::slotServerRead);
         connect(curr_mTcpSocket,&QTcpSocket::disconnected,
                 this,&MyTcpServer::slotClientDisconnected);
-        mTcpSocket->puch_back(curr_mTcpSocket);
+        mTcpSocket.push_back(curr_mTcpSocket);
     }
 }
 
 void MyTcpServer::slotServerRead(){
-    while(mTcpSocket->bytesAvailable()>0)
+    QTcpSocket* curr_mTcpSocket = (QTcpSocket*)sender();
+    while(curr_mTcpSocket->bytesAvailable()>0)
     {
-        QByteArray array =mTcpSocket->readAll();
-        mTcpSocket->write(array);
+        QByteArray array =curr_mTcpSocket->readAll();
+        curr_mTcpSocket->write(array);
     }
 }
 
 void MyTcpServer::slotClientDisconnected(){
-    mTcpSocket->close();
+    QTcpSocket* curr_mTcpSocket = (QTcpSocket*)sender();
+    curr_mTcpSocket->close();
+    curr_mTcpSocket->deleteLater();
 }
