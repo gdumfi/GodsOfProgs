148262203761775675759644ff4acef1c8c2dd8b
gdumfi <44841897+gdumfi@users.noreply.github.com>
2023-04-09 21:32:02 +0300

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -1,11 +1,20 @@
-#include <QCoreApplication>
-//#include <mysingleton.h>
 #include "mytcpserver.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("DRIVER = {SQL Server}; SERVER = (127.0.0.1); DATABASE = (sqlite)")
+    if (db.open)
+    {
+        qDebug()<<"opened";
+    }
+    else
+    {
+        qDebug()<<"closed";
+    }
+    
     MyTcpServer server;
     return a.exec();
 }
