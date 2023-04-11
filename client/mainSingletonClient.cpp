6c8aad52ab1c7ac647d224237698b59bb013cec8
gdumfi <44841897+gdumfi@users.noreply.github.com>
2023-04-11 20:43:07 +0300

    Delete client directory

--- client/mainSingletonClient.cpp
+++
@@ -1,8 +0,0 @@
-#include <QCoreApplication>
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    qDebug()<<SingletonClient::getInstance()->/*метод*/;
-    return a.exec();
-}
