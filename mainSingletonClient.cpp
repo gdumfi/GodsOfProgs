26d02643212756efb879948a19fd0de8ba372a11
gdumfi <44841897+gdumfi@users.noreply.github.com>
2023-05-31 12:56:03 +0300

    Delete mainSingletonClient.cpp

--- mainSingletonClient.cpp
+++
@@ -1,8 +0,0 @@
-#include <QCoreApplication>
-#include "singletondb.h"
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    qDebug()<<SingletonDB::getInstance()->queryToDB("select * from Users")/*метод*/;
-    return a.exec();
-}
